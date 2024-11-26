27cd5b19fc1f073bb2882e41c96b098eadb02e72
fprojectslib <createstuffadi@gmail.com>
2024-11-26 19:54:25 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,205 +1,133 @@
-import * as React from "react";
-import { StyleSheet, View, StatusBar, Platform } from "react-native";
-import { ControlBar } from "./ControlBar";
-import { EditingWindow } from "./EditingWindow";
-import * as ImageManipulator from "expo-image-manipulator";
-import { Processing } from "./Processing";
-import { useRecoilState, RecoilRoot } from "recoil";
-import {
-  processingState,
-  imageDataState,
-  editingModeState,
-  readyState,
-  ImageDimensions,
-} from "./Store";
-import { OperationBar } from "./OperationBar/OperationBar";
-import { UniversalModal } from "./UniversalModal";
-const noScroll = require("no-scroll");
-
-type EditorContextType = {
-  throttleBlur: boolean;
-  minimumCropDimensions: ImageDimensions;
-  fixedAspectRatio: number;
-  lockAspectRatio: boolean;
-  mode: Mode;
-  onCloseEditor: () => void;
-  onEditingComplete: (result: any) => void;
-  allowedTransformOperations?: TransformOperations[];
-  allowedAdjustmentOperations?: AdjustmentOperations[];
-};
-
-export const EditorContext = React.createContext<EditorContextType>({
-  throttleBlur: true,
-  minimumCropDimensions: {
-    width: 0,
-    height: 0,
-  },
-  fixedAspectRatio: 1.6,
-  lockAspectRatio: false,
-  mode: "full",
-  onCloseEditor: () => {},
-  onEditingComplete: () => {},
-});
-
-export type Mode = "full" | "crop-only";
-
-export type TransformOperations = "crop" | "rotate";
-export type AdjustmentOperations = "blur";
-export type EditingOperations = TransformOperations | AdjustmentOperations;
-
-export interface ImageEditorProps {
-  visible: boolean;
-  onCloseEditor: () => void;
-  imageUri: string | undefined;
-  fixedCropAspectRatio?: number;
-  minimumCropDimensions?: {
-    width: number;
-    height: number;
-  };
-  onEditingComplete: (result: any) => void;
-  lockAspectRatio?: boolean;
-  throttleBlur?: boolean;
-  mode?: Mode;
-  allowedTransformOperations?: TransformOperations[];
-  allowedAdjustmentOperations?: AdjustmentOperations[];
-  asView?: boolean;
-}
-
-function ImageEditorCore(props: ImageEditorProps) {
-  //
-  const {
-    mode = "full",
-    throttleBlur = true,
-    minimumCropDimensions = { width: 0, height: 0 },
-    fixedCropAspectRatio: fixedAspectRatio = 1.6,
-    lockAspectRatio = false,
-    allowedTransformOperations,
-    allowedAdjustmentOperations,
-  } = props;
-
-  const [, setImageData] = useRecoilState(imageDataState);
-  const [, setReady] = useRecoilState(readyState);
-  const [, setEditingMode] = useRecoilState(editingModeState);
-
-  // Initialise the image data when it is set through the props
-  React.useEffect(() => {
-    const initialise = async () => {
-      if (props.imageUri) {
-        const enableEditor = () => {
-          setReady(true);
-          // Set no-scroll to on
-          noScroll.on();
-        };
-        // Platform check
-        if (Platform.OS === "web") {
-          let img = document.createElement("img");
-          img.onload = () => {
+import * as ImageManipulator from 'expo-image-manipulator';
+import { useCallback, useEffect } from 'react';
+import { Modal, StatusBar, StyleSheet, View, Text } from 'react-native';
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { EditorContext } from './context/editor';
+import { ControlBar } from './ControlBar';
+import { EditingWindow } from './EditingWindow';
+import { Processing } from './Processing';
+import { editingModeState, editorOptionsState, imageDataState, isEditState, processingState, readyState, } from './Store';
+function ImageEditorCore(props) {
+    const { minimumCropDimensions = { width: 100, height: 100 }, fixedAspectRatio = 0.66666666666, onEditingCancel, onEditingComplete, imageUri = null, processingComponent, editorOptions, } = props;
+    const [options, setOptions] = useRecoilState(editorOptionsState);
+    const [imageData, setImageData] = useRecoilState(imageDataState);
+    const [, setReady] = useRecoilState(readyState);
+    const [, setEditingMode] = useRecoilState(editingModeState);
+    const [, setProcessing] = useRecoilState(processingState);
+    const [isEdit] = useRecoilState(isEditState);
+    const initialize = useCallback(async () => {
+        setProcessing(true);
+        if (imageUri) {
+            const { width: pickerWidth, height: pickerHeight } = await ImageManipulator.manipulateAsync(imageUri, []);
             setImageData({
-              uri: props.imageUri ?? "",
-              height: img.height,
-              width: img.width,
+                uri: imageUri,
+                width: pickerWidth,
+                height: pickerHeight,
             });
-            enableEditor();
-          };
-          img.src = props.imageUri;
-        } else {
-          const { width: pickerWidth, height: pickerHeight } =
-            await ImageManipulator.manipulateAsync(props.imageUri, []);
-          setImageData({
-            uri: props.imageUri,
-            width: pickerWidth,
-            height: pickerHeight,
-          });
-          enableEditor();
+            setReady(true);
+            setProcessing(false);
         }
-      }
+    }, []);
+    const onBackPress = () => {
+        if (!isEdit) {
+            onEditingCancel();
+        }
+        else {
+            setProcessing(true);
+            initialize().then(() => {
+                setEditingMode('crop');
+                setProcessing(false);
+            });
+        }
     };
-    initialise();
-  }, [props.imageUri]);
-
-  const onCloseEditor = () => {
-    // Set no-scroll to off
-    noScroll.off();
-    props.onCloseEditor();
-  };
-
-  React.useEffect(() => {
-    // Reset the state of things and only render the UI
-    // when this state has been initialised
-    if (!props.visible) {
-      setReady(false);
+    const onSave = () => {
+        onEditingComplete(imageData);
+    };
+    useEffect(() => {
+        initialize().then(setCustomStyles).catch(console.error);
+    }, [imageUri]);
+    function setCustomStyles() {
+        if (editorOptions) {
+            const custom = Object.assign({}, options);
+            Object.entries(editorOptions).forEach(([key, value]) => {
+                if (key) {
+                    if (typeof custom[key] === 'object' && custom[key] !== null) {
+                        custom[key] = { ...custom[key], ...value };
+                    }
+                    else {
+                        custom[key] = value;
+                    }
+                }
+            });
+            setOptions(custom);
+        }
     }
-    // Check if ther mode is set to crop only if this is the case then set the editingMode
-    // to crop
-    if (mode === "crop-only") {
-      setEditingMode("crop");
-    }
-  }, [props.visible]);
-
-  return (
-    <EditorContext.Provider
-      value={{
-        mode,
-        minimumCropDimensions,
-        lockAspectRatio,
-        fixedAspectRatio,
-        throttleBlur,
-        allowedTransformOperations,
-        allowedAdjustmentOperations,
-        onCloseEditor,
-        onEditingComplete: props.onEditingComplete,
-      }}
-    >
-      <StatusBar hidden={props.visible} />
-      {props.asView ? (
-        <ImageEditorView {...props} />
-      ) : (
-        <UniversalModal
-          visible={props.visible}
-          presentationStyle="fullScreen"
-          statusBarTranslucent
-        >
-          <ImageEditorView {...props} />
-        </UniversalModal>
-      )}
-    </EditorContext.Provider>
-  );
+    return (<EditorContext.Provider value={{
+            minimumCropDimensions,
+            fixedAspectRatio,
+            onBackPress,
+            onSave,
+            imageUri,
+        }}>
+      <StatusBar hidden={true}/>
+      <ImageEditorView processingComponent={processingComponent}/>
+    </EditorContext.Provider>);
 }
-
-export function ImageEditorView(props: ImageEditorProps) {
-  //
-  const { mode = "full" } = props;
-
-  const [ready, setReady] = useRecoilState(readyState);
-  const [processing, setProcessing] = useRecoilState(processingState);
-
-  return (
-    <>
-      {ready ? (
-        <View style={styles.container}>
-          <ControlBar />
+export function ImageEditorView({ processingComponent }) {
+    const [ready] = useRecoilState(readyState);
+    const [processing] = useRecoilState(processingState);
+    const { backgroundColor, controlBar } = useRecoilValue(editorOptionsState);
+    return (<>
+      {ready && (<View style={[styles.container, { backgroundColor }]}>
+          {(controlBar === null || controlBar === void 0 ? void 0 : controlBar.position) === 'top' && <ControlBar />}
           <EditingWindow />
-          {mode === "full" && <OperationBar />}
+          {(controlBar === null || controlBar === void 0 ? void 0 : controlBar.position) === 'bottom' && <ControlBar />}
+        </View>)}
+
+      {processing && <Processing customComponent={processingComponent}/>}
+        <View style={styles.tipsContainer}>
+            <Text style={styles.tipsHeader}>For best results:</Text>
+            <Text style={styles.tips}>- Make sure the image contains only one clothing item.</Text>
+            <Text style={styles.tips}>- Make sure no person is in the image.</Text>
+            <Text style={styles.tips}>- Make sure the clothing item is centered.</Text>
+            <Text style={styles.tips}>- Ensure good lighting and clear image with upright orientation.</Text>
         </View>
-      ) : null}
-      {processing ? <Processing /> : null}
-    </>
-  );
+    </>);
 }
-
-export function ImageEditor(props: ImageEditorProps) {
-  //
-
-  return (
-    <RecoilRoot>
-      <ImageEditorCore {...props} />
-    </RecoilRoot>
-  );
+export function ImageEditor({ isVisible, ...props }) {
+    return (<Modal visible={isVisible} style={styles.modalContainer}>
+      <RecoilRoot>
+        <ImageEditorCore {...props}/>
+      </RecoilRoot>
+    </Modal>);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#222",
-  },
+    container: {
+        flex: 0.8,
+    },
+    modalContainer: {
+        flex: 1,
+        zIndex: 1,
+    },
+    tipsContainer: {
+        backgroundColor: 'white',
+        padding: 20,
+        flex: 0.2,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: 'gray',
+    },
+    tipsHeader: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical: 10,
+        marginLeft: 10,
+    },
+    tips: {
+        fontSize: 16,
+        color: 'gray',
+        marginTop: 5,
+        marginLeft: 5,
+    },
 });
+//# sourceMappingURL=index.js.map
